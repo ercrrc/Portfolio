707b354a92e08aabcd4f62e028f9e822ed768579
ercrrc <134560333+ercrrc@users.noreply.github.com>
2024-02-14 20:32:15 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from "../../components/Avatar"
+import Paragrafo from "../../components/Paragrafo"
 import Titulo from "../../components/Titulo"
+
+import { Descricao, BotaoTema, SidebarContainer } from "./styles"
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/ercrrc.png" />
+    <SidebarContainer>
+    <Avatar />
     <Titulo fontSize={20}>Erivelton Correia</Titulo>
+    <Paragrafo tipo="secundario" fontSize={16}>
+      ercrrc
+    </Paragrafo>
+    <Descricao tipo="principal" fontSize={12}>
+      Engenheiro front-end
+      </Descricao>
+    <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
